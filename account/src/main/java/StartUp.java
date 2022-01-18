4e5b4376f205d245b4ce212f2a253cc3970ef632
Nafiu <s184174@student.dtu.dk>
2022-01-18 12:45:49 +0100

    corrected name convention

--- account/src/main/java/StartUp.java
+++ account/src/main/java/StartUp.java
@@ -3,7 +3,7 @@ import domain.storage.InMemory;
 import messaging.implementations.RabbitMqQueue;
 
 public class StartUp {
-// test comment
+
     public void startUp() {
         System.out.println("startup of account microservice");
         var mq = new RabbitMqQueue("localhost");
