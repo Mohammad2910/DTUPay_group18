7a9df27d01476b65de9330c166a76e4ef3eb1c17
Maria Eleni Kostopoulou <marialena.kostopoulou@outlook.com>
2022-01-19 23:12:40 +0100

    Remove leftover handler

--- account/src/main/java/adapters/AccountController.java
+++ account/src/main/java/adapters/AccountController.java
@@ -21,6 +21,7 @@ public class AccountController {
     public AccountController(MessageQueue queue, InMemory memory) {
         this.queue = queue;
         accountLogic = new DTUPayAccountBusinessLogic(memory);
+
         // todo: make handlers for each event Account need to look at
         queue.addHandler("CreateCustomerAccount", this::handleCreateCustomerAccountRequest);
         queue.addHandler("CreateMerchantAccount", this::handleCreateMerchantAccountRequest);
@@ -69,7 +70,6 @@ public class AccountController {
         Event tokenAssign = new Event("CreateCustomerWithTokens", new Object[] {requestId, account.getId(), null});
         queue.publish(tokenAssign);
 
-
         // Publish response event for facade
         Event accCreationSucceeded = new Event("CustomerAccountCreated", new Object[] {requestId, account, null});
         queue.publish(accCreationSucceeded);
@@ -101,6 +101,7 @@ public class AccountController {
         String requestId = event.getArgument(0, String.class);
         String errorMessage = event.getArgument(2, String.class);
         this.publishPropagatedError("MerchantAccountCreateFailed", requestId, errorMessage);
+
         // Create account
         DTUPayAccount account = event.getArgument(1, DTUPayAccount.class);
         try {
@@ -144,7 +145,6 @@ public class AccountController {
         // Delete account
         DTUPayAccount account = event.getArgument(1, DTUPayAccount.class);
         try {
-            //DTUPayAccount account = accountLogic.get(accountId);
             accountLogic.delete(account);
         } catch (NoSuchAccountException e) {
             // Publish response event for facade with propagated error message
@@ -158,7 +158,7 @@ public class AccountController {
     }
 
     /**
-     * Consumes events of type ExtractBankAccounts and published an event in queue BankAccountsExported
+     * Consumes events of type ExtractBankAccounts and published an event in queue BankAccountsExported/BankAccountsExportFailed
      *
      *  Consumed event arguments:
      * 1. requestId
@@ -184,15 +184,15 @@ public class AccountController {
         this.publishPropagatedError("BankAccountsExportFailed", requestId, errorMessage);
 
         // Get account
-        PaymentPayload paymentEvent = event.getArgument(1, PaymentPayload.class);
+        PaymentPayload paymentPayload = event.getArgument(1, PaymentPayload.class);
         try {
             // Set customer bank account to payment event
-            DTUPayAccount customerAccount = accountLogic.get(paymentEvent.getCustomerId());
-            paymentEvent.setCustomerBankAccount(customerAccount.getDtuBankAccount());
+            DTUPayAccount customerAccount = accountLogic.get(paymentPayload.getCustomerId());
+            paymentPayload.setCustomerBankAccount(customerAccount.getDtuBankAccount());
 
             // Set merchant bank account to payment event
-            DTUPayAccount merchantAccount = accountLogic.get(paymentEvent.getMerchantId());
-            paymentEvent.setMerchantBankAccount(merchantAccount.getDtuBankAccount());
+            DTUPayAccount merchantAccount = accountLogic.get(paymentPayload.getMerchantId());
+            paymentPayload.setMerchantBankAccount(merchantAccount.getDtuBankAccount());
         } catch (NoSuchAccountException e) {
             // Publish response event for the payment microservice
             Event accExtractedFailed = new Event("BankAccountsExportFailed", new Object[] {requestId, null, e.getMessage()});
@@ -200,7 +200,7 @@ public class AccountController {
         }
 
         // Publish payment event for the payment microservice to complete the payment
-        Event accExportedSucceeded = new Event("BankAccountsExported", new Object[] {requestId, paymentEvent, null});
+        Event accExportedSucceeded = new Event("BankAccountsExported", new Object[] {requestId, paymentPayload, null});
         queue.publish(accExportedSucceeded);
     }
 
