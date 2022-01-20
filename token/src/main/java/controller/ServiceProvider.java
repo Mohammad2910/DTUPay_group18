d1995b96eab8200dac371ae8396fd741f29cf111
didi <s147197@student.dtu.dk>
2022-01-20 10:34:07 +0100

    small doc correction

--- token/src/main/java/controller/ServiceProvider.java
+++ token/src/main/java/controller/ServiceProvider.java
@@ -8,12 +8,12 @@ import storage.TokenStorage;
 
 /**
  * @author Renjue, Christian and David
- *                                                  !!This is the Service provider class!!
- *                                          This class should be instantiated in the main method
- *                      The Responsibility of this class is to provide the right dependency injection for the required classes.
- *                                              To add a dependency:
- *                                                      - make an object of the wanted class
- *                                                      - inject the instantiation of the object in the parameter of the dependent class.
+ * !!This is the Service provider class!!
+ *   This class should be instantiated in the main method
+ *   The Responsibility of this class is to provide the right dependency injection for the required classes.
+ *    To add a dependency:
+ *         - make an object of the wanted class
+ *         - inject the instantiation of the object in the parameter of the dependent class.
  */
 public class ServiceProvider {
 
