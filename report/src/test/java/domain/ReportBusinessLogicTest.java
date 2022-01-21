1a3c11fece547d83b068c58dc9d9ca5792e58db3
Nafiu <s184174@student.dtu.dk>
2022-01-21 19:02:10 +0100

    test

--- report/src/test/java/domain/ReportBusinessLogicTest.java
+++ report/src/test/java/domain/ReportBusinessLogicTest.java
@@ -1,8 +1,8 @@
-package java.domain;
+package domain;
 
 import java.util.List;
 
-import domain.ReportBusinessLogic;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
